e491f37156a2eed19599b166f3675cf5e472dc71
Steven Maddela <70993825+stevenmaddela@users.noreply.github.com>
2024-04-22 16:46:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import numpy as np  # Import numpy library
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "https://comet-scraper1.vercel.app"}})
+CORS(app, resources={r"/*": {"origins": "*"}})
 
 
 @app.route('/')
