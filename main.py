518b7d124f1e19284391530b1dd02ca8102cd293
Steven Maddela <70993825+stevenmaddela@users.noreply.github.com>
2024-04-23 17:04:43 -0500

    Update main.py

--- main.py
+++ main.py
@@ -82,7 +82,7 @@ def get_stock_info():
  
 @app.route('/recommendations', methods=['GET'])
 def get_recommendations():
-    def pick_stocks_based_on_distribution(total_stocks=20, existing_stocks=[]):
+    def pick_stocks_based_on_distribution(total_stocks=100, existing_stocks=[]):
         picked_stocks = []
 
         # Read all stocks from StockInfo.txt
@@ -133,10 +133,10 @@ def get_recommendations():
 
     stats_array = []
 
-    # Fetch stats for each picked stock individually
-    for stock in picked_stocks:
-        stats = get_stats(stock)
-        stats_array.append(stats)
+    # Fetch stats for the picked stocks using multithreading
+    with ThreadPoolExecutor() as executor:
+        for stats in executor.map(get_stats, picked_stocks):
+            stats_array.append(stats)
 
     # Sort the stats_array based on the absolute difference between each stock's price and the average_price
     sorted_stats = sorted(stats_array, key=lambda x: abs(x[1] - average_price))
@@ -307,208 +307,11 @@ def get_sentiment():
     chartPointCt =  len(close_prices_list)
 
 
-    news_articles = []
-    try:
-        news_articles = stock.news
-        if news_articles is not None:
-        # Process news articles
-            pass
-        else:
-            news_articles = []
-        # Handle case where no news articles are available
-            pass
-    except Exception as e:
-    # Handle any exceptions that occur during news retrieval
-        print(f"An error occurred while retrieving news articles: {e}")
-
-        
-    GetStockInfo = yf.Ticker(stock_ticker)
-
-    long_business_summary = "LBS"
-    if 'longBusinessSummary' in GetStockInfo.info:
-        long_business_summary = GetStockInfo.info['longBusinessSummary']
-    else:
-        print("No 'longBusinessSummary' found in the dictionary.")# get all key value pairs that are available
-
-
-    historical_data = stock.history(period='2d', interval='1d')
-
-    # Access the closing price from yesterday (second-to-last data point)
-    yesterday_close = float(historical_data['Close'].iloc[-2])
-
-    # Get the intraday data for the current day (as you've already done)
-    intraday_data = stock.history(period='1d', interval='1m')
-
-    # Access the most recent closing price (current value)
-# Access the most recent closing price (current value)
-    current_value = float(intraday_data['Close'].iloc[-1])  # Convert to float
-
-    # Calculate the change in dollars
-    change_in_dollars = current_value - yesterday_close
-
-    # Calculate the percent change
-    percent_change = (change_in_dollars / yesterday_close) * 100
-
-    article_links = []
-    article_texts = []
-    article_titles = []
-    article_publishers = []
-    article_dates = []
-    articleCt = 0
-
-    if(len(news_articles) > 1):
-
-        for i, article in enumerate(news_articles):
-            if i >= 15:
-                break
-
-            try:
-                # Fetch the article content using requests and BeautifulSoup
-                response = requests.get(article['link'])
-                soup = BeautifulSoup(response.text, 'html.parser')
-
-                # Find and print the article body text
-                article_body = soup.find('div', class_='caas-body')
-                if article_body:
-                    article_title = article['title']
-                    article_text = article_body.get_text()
-                    article_texts.append((article_title, article_text))
-                    articleCt = articleCt + 1
-                    article_links.append(article['link'])
-                    article_publishers.append(article['publisher'])
-                    providerpublishtime = datetime.utcfromtimestamp( article['providerPublishTime'])
-                    current_time = datetime.utcnow()
-                    time_difference = current_time - providerpublishtime
-
-                    # If the article was published on the same day, display the time in hours
-                    if time_difference.days == 0:
-                        hours_ago = time_difference.seconds // 3600  # Convert seconds to hours
-                        if hours_ago == 0:
-                            # If less than an hour ago, calculate and display minutes
-                            minutes_ago = (time_difference.seconds % 3600) // 60
-                            if minutes_ago == 0:
-                                formatted_time = "Just now"
-                            elif minutes_ago == 1:
-                                formatted_time = "1 minute ago"
-                            else:
-                                formatted_time = f"{minutes_ago} minutes ago"
-                        elif hours_ago == 1:
-                            formatted_time = "1 hour ago"
-                        else:
-                            formatted_time = f"{hours_ago} hours ago"
-                    elif time_difference.days == 1:
-                        formatted_time = "1 day ago"
-                    else:
-                        # If not the same day, display days
-                        formatted_time = f"{time_difference.days} days ago"
-
-                    article_dates.append(formatted_time)
-                    #print("-" * 50)
-                else:
-                    print("Article body not found on the page.")
-
-            except Exception as e:
-                print(f"Error processing article: {str(e)}")
-
-    vader = SentimentIntensityAnalyzer() # or whatever you want to call it
-
-    data = []
-    scores_list = []
-
-    if(len(article_texts) > 1):
-        # Iterate through the list of article texts and titles
-        for i, (article_title, article_text) in enumerate(article_texts):
-            # Concatenate the title and text, treating the title as the first sentence
-            combined_text = article_title + ' ' + article_text
-            article_titles.append(article_title)
-            # Analyze the sentiment of the combined text
-            sentiment_scores = vader.polarity_scores(combined_text)
-
-            # Extract sentiment scores
-            compound_score = sentiment_scores['compound']
-            positive_score = sentiment_scores['pos']
-            negative_score = sentiment_scores['neg']
-            neutral_score = sentiment_scores['neu']
-            scores = [
-                ('positive_score', positive_score),
-                ('negative_score', negative_score),
-                ('neutral_score', neutral_score),
-                ('compound_score', compound_score)
-            ]
-            scores_list.append(scores)
-
-            # Determine sentiment based on the compound score
-            if compound_score >= 0.05:
-                sentiment = 'pos'
-            elif compound_score <= -0.05:
-                sentiment = 'neg'
-            else:
-                sentiment = 'neu'
-
-            # Append data to the list
-            data.append([article_title, sentiment_scores, compound_score, sentiment])
-
-    combined_triplets = None
-    mean_neu = 0
-    mean_pos = 0
-    mean_neg = 0
-    overall_score = 'nothing'
-    average_compound_score = 0
-    if (len(data) > 1):
-        # Create a DataFrame from the data list
-        df = pd.DataFrame(data, columns=['Article Title', 'Sentiment Scores', 'Compound Score', 'Sentiment'])
-
-        # Calculate the mean values for 'neg', 'neu', and 'pos'
-        mean_neg = df['Sentiment Scores'].apply(lambda x: x['neg']).mean()
-        mean_neu = df['Sentiment Scores'].apply(lambda x: x['neu']).mean()
-        mean_pos = df['Sentiment Scores'].apply(lambda x: x['pos']).mean()
-        
-        # Calculate the mean 'Compound Score'
-        average_compound_score = df['Compound Score'].mean()
-
-
-        # Derive the overall sentiment score based on average polarity
-        if average_compound_score >= 0.05:
-            overall_score = 'positive'
-        elif average_compound_score <= -0.05:
-            overall_score = 'negative'
-        else:
-            overall_score = 'neutral'
-
-        combined_triplets = [{'href': link, 'title': title, 'score': score, 'info': info, 'date' : date} for link, title, score, info, date in zip(article_links, article_titles, scores_list, article_publishers, article_dates)]
-
-        # Assuming mean_neg, mean_neu, mean_pos, and average_compound_score are potentially nullable variables
-
-    if combined_triplets is None:
-    # Set combined_triplets to a default value
-        combined_triplets = [2,3]  # Or any other appropriate default value
-
-    if ((len(data) > 1) and (len(news_articles) > 1) and (len(article_texts) > 1) ):
-        return jsonify({
-            'Stock' : stock_ticker,
-            'Value' : current_value,
-            'yClose' : yesterday_close,
-            'dChange' : change_in_dollars,
-            'pChange' : percent_change,
-            'LBS' : long_business_summary,
-            'Close Prices': close_prices_list,
-            'ChartPointCt' : chartPointCt,
-        })
+    
 
     return jsonify({
         'Stock' : stock_ticker,
-        'Value' : current_value,
-        'Count' : articleCt,
-        'Articles' : combined_triplets,
-        'yClose' : yesterday_close,
-        'dChange' : change_in_dollars,
-        'pChange' : percent_change,
-        'LBS' : long_business_summary,
-        'Neg': mean_neg,
-        'Neu': mean_neu,
-        'Pos': mean_pos,
-        'Compound Score': average_compound_score,
-        'Overall Sentiment': overall_score,
+        
         'Close Prices': close_prices_list,
         'ChartPointCt' : chartPointCt,
     })
