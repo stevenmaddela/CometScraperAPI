d2346f9e0aa233430cfe2be3d0fbeb79d67a4689
Steven Maddela <70993825+stevenmaddela@users.noreply.github.com>
2024-04-22 21:04:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -109,6 +109,7 @@ def get_recommendations():
 
     def pick_stocks_based_on_distribution(sector_distribution, total_stocks=100, existing_stocks=[]):
         picked_stocks = []
+        stocks_in_sector = []
         # Pick stocks based on sector distribution percentages
         for sector, percentage in sector_distribution.items():
             num_stocks = int(total_stocks * (percentage / 100))
